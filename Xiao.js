99d3be13ec9d9abe7e759604bd714f4ddb0df4e8
rare_akuma <magumboss1@gmail.com>
2018-08-14 16:15:35 -0500

    yes

--- Xiao.js
+++ Xiao.js
@@ -1,55 +1,55 @@
-const { XIAO_TOKEN, OWNERS, XIAO_PREFIX, INVITE } = process.env;
+require('dotenv').config();
 const path = require('path');
 const { CommandoClient } = require('discord.js-commando');
 const client = new CommandoClient({
-	commandPrefix: XIAO_PREFIX,
-	owner: OWNERS.split(','),
-	invite: INVITE,
-	disableEveryone: true,
-	unknownCommandResponse: false,
-	disabledEvents: ['TYPING_START']
+    commandPrefix: process.env.XIAO_PREFIX,
+    owner: process.env.OWNERS.split(','),
+    invite: process.env.INVITE,
+    disableEveryone: true,
+    unknownCommandResponse: false,
+    disabledEvents: ['TYPING_START']
 });
 const activities = require('./assets/json/activity');
 
 client.registry
-	.registerDefaultTypes()
-	.registerTypesIn(path.join(__dirname, 'types'))
-	.registerGroups([
-		['util', 'Utility'],
-		['info', 'Discord Information'],
-		['random', 'Random Response'],
-		['single', 'Single Response'],
-		['events', 'Events'],
-		['search', 'Search'],
-		['analyze', 'Analyzers'],
-		['games', 'Games'],
-		['voice', 'Voice Channel'],
-		['image-edit', 'Image Manipulation'],
-		['avatar-edit', 'Avatar Manipulation'],
-		['text-edit', 'Text Manipulation'],
-		['number-edit', 'Number Manipulation'],
-		['other', 'Other'],
-		['roleplay', 'Roleplay']
-	])
-	.registerDefaultCommands({
-		help: false,
-		ping: false,
-		prefix: false,
-		commandState: false
-	})
-	.registerCommandsIn(path.join(__dirname, 'commands'));
+    .registerDefaultTypes()
+    .registerTypesIn(path.join(__dirname, 'types'))
+    .registerGroups([
+        ['util', 'Utility'],
+        ['info', 'Discord Information'],
+        ['random', 'Random Response'],
+        ['single', 'Single Response'],
+        ['events', 'Events'],
+        ['search', 'Search'],
+        ['analyze', 'Analyzers'],
+        ['games', 'Games'],
+        ['voice', 'Voice Channel'],
+        ['image-edit', 'Image Manipulation'],
+        ['avatar-edit', 'Avatar Manipulation'],
+        ['text-edit', 'Text Manipulation'],
+        ['number-edit', 'Number Manipulation'],
+        ['other', 'Other'],
+        ['roleplay', 'Roleplay']
+    ])
+    .registerDefaultCommands({
+        help: false,
+        ping: false,
+        prefix: false,
+        commandState: false
+    })
+    .registerCommandsIn(path.join(__dirname, 'commands'));
 
 client.on('ready', () => {
-	console.log(`[READY] Logged in as ${client.user.tag}! (${client.user.id})`);
-	client.setInterval(() => {
-		const activity = activities[Math.floor(Math.random() * activities.length)];
-		client.user.setActivity(activity.text, { type: activity.type });
-	}, 60000);
+    console.log(`[READY] Logged in as ${client.user.tag}! (${client.user.id})`);
+    client.setInterval(() => {
+        const activity = activities[Math.floor(Math.random() * activities.length)];
+        client.user.setActivity(activity.text, { type: activity.type });
+    }, 60000);
 });
 
 client.on('disconnect', event => {
-	console.error(`[DISCONNECT] Disconnected with code ${event.code}.`);
-	process.exit(0);
+    console.error(`[DISCONNECT] Disconnected with code ${event.code}.`);
+    process.exit(0);
 });
 
 client.on('commandRun', command => console.log(`[COMMAND] Ran command ${command.groupID}:${command.memberName}.`));
@@ -60,9 +60,9 @@ client.on('warn', err => console.warn('[WARNING]', err));
 
 client.on('commandError', (command, err) => console.error('[COMMAND ERROR]', command.name, err));
 
-client.login(XIAO_TOKEN);
+client.login(process.env.XIAO_TOKEN);
 
 process.on('unhandledRejection', err => {
-	console.error('[FATAL] Unhandled Promise Rejection.', err);
-	process.exit(1);
+    console.error('[FATAL] Unhandled Promise Rejection.', err);
+    process.exit(1);
 });
